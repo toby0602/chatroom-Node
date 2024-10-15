acb497a462be4adc388c18ae2babcdb3f5330e4c
toby <shan5201010@gmail.com>
2024-10-15 11:51:27 +0800

    commit

--- server-mongo.js
+++ server-mongo.js
@@ -19,6 +19,7 @@ app.use((req, res, next) => {
     console.log(`Request URL: ${req.url}`);
     next();
 });
+console.log(111222, process.env.MONGODB_URI);
 
 // 連接到 MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
